bd9a5a5a1a45cbfd4f9ecc15f2575e8c79306141
Bruin.Yxl <814878862@qq.com>
2018-06-29 17:31:36 +0800

    fix

--- src/helpers.php
+++ src/helpers.php
@@ -7,7 +7,7 @@ if (!function_exists('curl_post')) {
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
-        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+//        curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
